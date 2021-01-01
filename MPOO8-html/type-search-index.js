e7d444d2cd7c5a908b07e37e6aeca017786e66d6
Ironspider150501 <53962137+Ironspider150501@users.noreply.github.com>
2020-12-31 21:41:54 -0600

    Add files via upload

---
+++ MPOO8-html/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Cajero","l":"Cajero"},{"p":"Cajero","l":"Cuenta"},{"p":"<Unnamed>","l":"MPOO8"},{"p":"Cajero","l":"SaldoInsuficienteException"}]
